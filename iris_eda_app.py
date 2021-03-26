80871b31d9f781d27e49c8384335ca902182ffad
Quarky123 <>
2021-03-26 20:31:38 +0800

    s

--- iris_eda_app.py
+++ iris_eda_app.py
@@ -1,9 +1,11 @@
+# heroku create iris-eda-app123
+
 import streamlit as st
 
 def main():
     # simple EDA app
     st.title("Iris EDA App with streamlit")
     st.subheader("Streamlit is Cool")
-    
+
 if __name__ == '__main__':
     main()
